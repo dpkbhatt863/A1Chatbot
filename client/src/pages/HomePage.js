51093222933e49a5e99e3b4aee2012104f229ad5
Deepak Bhatt <51474126+dpkbhatt863@users.noreply.github.com>
2024-11-07 10:50:10 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 import aiAssistantImage from '../assets/ai-assistant.png'; 
 
-const url = "https://a1chatbotbackend.onrender.com"
+const url = "https://a1chatbot.onrender.com"
 
 function HomePage({ user }) {
   return (
